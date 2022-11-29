c491c756a444cb79ac2420ca84fcf59d1d897f3b
chen <409631293@gms.tku.edu.tw>
2022-11-29 17:47:31 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({Key? key}) : super(key: key);
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  bool takeOff = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animation'),
+      ),
+      body: SizedBox(
+        child: Stack(
+          children: <Widget>[
+            AnimatedPositioned(
+              left: 100.0,
+              right: 100.0,
+              top: takeOff ? 100.0 : 400.0,
+              duration: const Duration(seconds: 2),
+              curve: Curves.fastOutSlowIn,
+              child: const Icon(
+                  Icons.airplanemode_active,
+                  color: Colors.blue,
+                  size: 48.0
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton.icon(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Colors.blue),
+                  foregroundColor: MaterialStateProperty.all(
+                      Colors.white),
+                ),
+                icon: const Icon(Icons.airplanemode_active, color: Colors.white),
+                label: const Text('起飛'),
+                onPressed: () => {
+                  setState(() {
+                    takeOff = !takeOff;
+                  })
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
